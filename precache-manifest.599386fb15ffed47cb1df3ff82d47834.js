c86af81f1d11402858052da7f7243e45e0aa1dd6
moe9195 <moerahmeh95@hotmail.com>
2020-03-28 23:42:06 +0300

    Updates

--- precache-manifest.599386fb15ffed47cb1df3ff82d47834.js
+++ precache-manifest.599386fb15ffed47cb1df3ff82d47834.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0653f76cc1a951ce2ff4f91a2a659cda",
+    "revision": "dabad8a5155a7fe6a71fc355a1626445",
     "url": "/whack-a-bebsi/index.html"
   },
   {
-    "revision": "f7ce9618d8b62ee5363c",
-    "url": "/whack-a-bebsi/static/css/main.97e985f1.chunk.css"
+    "revision": "90acf526e1acd05db8ca",
+    "url": "/whack-a-bebsi/static/css/main.9df4981d.chunk.css"
   },
   {
     "revision": "6dd1e3af7915132190d5",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/whack-a-bebsi/static/js/2.603c0b29.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f7ce9618d8b62ee5363c",
-    "url": "/whack-a-bebsi/static/js/main.4ca05a56.chunk.js"
+    "revision": "90acf526e1acd05db8ca",
+    "url": "/whack-a-bebsi/static/js/main.0fd016fd.chunk.js"
   },
   {
     "revision": "b86582a7944b6d2c2d9b",
